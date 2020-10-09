eec2c63827f25132b1c203927cc9a45671d98ce6
dabhoang <48900472+dabhoang@users.noreply.github.com>
2020-10-09 05:42:08 -0700

    refactored code and added comments

--- rostersearch.py
+++ rostersearch.py
@@ -1,54 +1,68 @@
 import sys
 import csv
 
-if(len(sys.argv) != 4):
-    print("Please enter arguments of the format...")
-    exit()
-
-roster = open(sys.argv[3], 'r', encoding = 'utf-8')
-students = list(csv.reader(roster))
-
-if sys.argv[1] == "-name":
-    print("name")
-    search_pattern = sys.argv[2].lower()
+def search_name(pattern): #searches for pattern in all students' first names and last names
+    print("Searching for all students with \"" + sys.argv[2] + "\" in their first name or last name")
     for student in students[1:]:
-        
-        student_firstname = student[0].lower()
-        student_lastname = student[1].lower()
-        print(search_pattern)
-        if(student_firstname.find(search_pattern) != -1):
+        student_firstname = student[0].lower() #gets the student's first name from csv file and ensures case insensitivity
+        student_lastname = student[1].lower() #gets the student's last name from csv file and ensures case insensitivity
+        print(pattern)
+        if(student_firstname.find(pattern) != -1): #determines if the pattern exists in the first name and prints the student if so
             print(student)
-        if(student_lastname.find(search_pattern) != -1):
+        if(student_lastname.find(pattern) != -1): #determines if the pattern exists in the last name and prints the student if so
             print(student)
 
-elif sys.argv[1] == "-email":
-    print("email")
-    search_pattern = sys.argv[2].lower()
+def search_email(pattern): #searches for pattern in all students' emails without "@ucdavis.edu" suffix
+    print("Searching for all students with \"" + sys.argv[2] + "\" in their email without the \"@ucdavis.edu\" suffix")
     for student in students[1:]:
-        emailbase = student[2].replace("@ucdavis.edu","").lower()
-        if(emailbase.find(search_pattern) != -1):
+        emailbase = student[2].replace("@ucdavis.edu","").lower() #gets rid of "@ucdavis.edu" suffix and ensures case insensitivity
+        if(emailbase.find(search_pattern) != -1): #determines if the pattern exists in the email base and prints the student if so
             print(student)
-elif sys.argv[1] == "-gpa":
+
+
+def search_gpa(gpa): #searches for students with certain gpa's or gpa's in the specified range based on suffix
     print("gpa")
+    if gpa[-1] == '+': #suffix is a '+', print all students with gpa's >= the entered gpa
+        gpa = sys.argv[2].replace("+","") #pulls gpa from csv file and gets rid of suffix
+        print(gpa)
+        for student in students[1:]:
+            if student[3] >= gpa: #print all students with gpa's greater then or equal to the entered gpa
+                print(student)
+
+    elif gpa[-1] == '-': #suffix is a '-', print all students with gpa's <= the entered gpa
+        gpa = sys.argv[2].replace("-","") #pulls gpa from csv file and gets rid of suffix
+        print(gpa)
+        for student in students[1:]: 
+            if student[3] <= gpa: #print all students with gpa's less than or equal to the entered gpa
+                print(student)
+
+    else: #no suffix
+        for student in students[1:]: 
+            if student[3] == gpa: #print all students with gpa's equal to the entered gpa
+                print(student) 
+
+
+#main program execution begins
+
+if(len(sys.argv) != 4): #makes sure that the number of arguments are correct
+    print("Please enter arguments of the format...") 
+    exit()
     
-    if sys.argv[2][-1] == '+':
-        gpa = sys.argv[2].replace("+","")
-        print(gpa)
-        for student in students[1:]:
-            if student[3] >= gpa:
-                print(student)
+if(sys.argv[3].endswith(".csv") is False): #last command line argument should be the name of the roster file
+    print("last command line argument must be a csv file containing the roster with a suffix of \".csv\"")
 
-    elif sys.argv[2][-1] == '-':
-        gpa = sys.argv[2].replace("-","")
-        print(gpa)
-        for student in students[1:]:
-            if student[3] <= gpa:
-                print(student)
-    else:
-        for student in students[1:]:
-            if student[3] == gpa:
-                print(student)
+roster = open(sys.argv[3], 'r', encoding = 'utf-8') #open the csv file
+students = list(csv.reader(roster)) #students is a list of all students
+search_pattern = sys.argv[2].lower() #ensures that our searches are case insensitive
 
-else: print("Please enter -name -email or -gpa")
+#determines the function for appropriate column(s) to search based on command
+if sys.argv[1] == "-name": #command is “-name <pattern>”
+    search_name(search_pattern)
+elif sys.argv[1] == "-email": #command is “-email <pattern>”
+    search_email(search_pattern)
+elif sys.argv[1] == "-gpa": #command is “-gpa <gpa>[+-]”
+    search_gpa(search_pattern)
 
-roster.close()
+else: print("Please enter -name -email or -gpa") #first command line argument after rostersearch.py should be -name, -email, or -gpa
+
+roster.close() #close the csv file
