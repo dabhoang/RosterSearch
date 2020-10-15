ce2602ae831bd56d6bf7f955bcd8acbbe7f61273
dabhoang <48900472+dabhoang@users.noreply.github.com>
2020-10-15 00:32:46 -0700

    added more test cases

--- rostertest.py
+++ rostertest.py
@@ -11,7 +11,7 @@ class TestRoster(TestCase):
 
     #test the search_name() function
 
-    def test_name0(self): 
+    def test_search_name_empty_file(self): 
         roster = open("roster.csv", 'w+', encoding = 'utf-8', newline="") #open the csv file
         people_in_class = list(csv.reader(roster)) #students is a list of all students
         expected = "Searching for all students with \"dab\" in their first name or last name:\n"
@@ -20,7 +20,19 @@ class TestRoster(TestCase):
             self.assertEqual(actual_results.getvalue(), expected)
         roster.close()
 
-    def test_name1(self): 
+    def test_search_name_empty_roster_with_column_titles(self): 
+        roster = open("roster.csv", 'w+', encoding = 'utf-8', newline="") #open the csv file
+        roster.truncate(0)
+        writer = csv.writer(roster)
+        writer.writerow(["First Name", "Last Name", "Email", "GPA"])
+        people_in_class = list(csv.reader(roster))
+        expected = "Searching for all students with \"irs\" in their first name or last name:\n"
+        with patch('sys.stdout', new = StringIO()) as actual_results:
+            search_commands.search_name("irs", people_in_class)
+            self.assertEqual(actual_results.getvalue(), expected)
+        roster.close()
+        
+    def test_search_name_roster_size_1_with_no_students_printed(self): 
         roster = open("roster.csv", 'w+', encoding = 'utf-8', newline="") #open the csv file
         roster.truncate(0)
         writer = csv.writer(roster)
@@ -34,7 +46,7 @@ class TestRoster(TestCase):
         roster.close()
         
 
-    def test_name2(self): 
+    def test_search_name_roster_size_1_with_1_student_printed(self): 
         roster = open("roster.csv", 'w+', encoding = 'utf-8', newline="") #open the csv file
         roster.truncate(0)
         writer = csv.writer(roster)
@@ -49,7 +61,28 @@ class TestRoster(TestCase):
             self.assertEqual(actual_results.getvalue(), expected)
         read_roster.close()
 
-    def test_name3(self): 
+    def test_name_roster_size_5_with_some_students_printed_lowercase_pattern(self): 
+        roster = open("roster.csv", 'w+', encoding = 'utf-8', newline="") #open the csv file
+        roster.truncate(0)
+        writer = csv.writer(roster)
+        writer.writerow(["First Name", "Last Name", "Email", "GPA"])
+        writer.writerow(["Dan", "Hoang", "danhoang@ucdavis.edu", "3.0"])
+        writer.writerow(["John", "Smith", "jsmith@ucdavis.edu", "3.78"])
+        writer.writerow(["Joey", "Johnson", "jjohnson@ucdavis.edu", "2.2"])
+        writer.writerow(["Eddie", "Jacobs", "ejacobs4@ucdavis.edu", "4.0"])
+        writer.writerow(["Jimmy", "Graham", "jgraham@ucdavis.edu", "3.22"])
+        roster.close()
+        read_roster = open("roster.csv", 'r+', encoding = 'utf-8') #open the csv file
+        people_in_class = list(csv.reader(x.replace('\0', '') for x in read_roster))
+        expected = "Searching for all students with \"jo\" in their first name or last name:\n" 
+        expected = expected + "[\'John\', \'Smith\', \'jsmith@ucdavis.edu\', '3.78']\n"
+        expected = expected + "[\'Joey\', \'Johnson\', \'jjohnson@ucdavis.edu\', '2.2']\n"
+        with patch('sys.stdout', new = StringIO()) as actual_results:
+            search_commands.search_name("jo", people_in_class)
+            self.assertEqual(actual_results.getvalue(), expected)
+        read_roster.close()
+
+    def test_name_roster_size_5_with_some_students_printed_uppercase_pattern(self): 
         roster = open("roster.csv", 'w+', encoding = 'utf-8', newline="") #open the csv file
         roster.truncate(0)
         writer = csv.writer(roster)
@@ -73,7 +106,7 @@ class TestRoster(TestCase):
     #--------------------------------------
     #test the search_email() function
 
-    def test_email0(self): 
+    def test_search_email_empty_roster(self): 
         roster = open("roster.csv", 'w+', encoding = 'utf-8', newline="") #open the csv file
         people_in_class = list(csv.reader(roster)) #students is a list of all students
         expected = "Searching for all students with \"dab\" in their email without the \"@ucdavis.edu\" suffix:\n"
@@ -82,7 +115,7 @@ class TestRoster(TestCase):
             self.assertEqual(actual_results.getvalue(), expected)
         roster.close()
 
-    def test_email1(self): 
+    def test_search_email_roster_size_1_with_no_students_printed(self): 
         roster = open("roster.csv", 'w+', encoding = 'utf-8', newline="") #open the csv file
         roster.truncate(0)
         writer = csv.writer(roster)
@@ -95,7 +128,7 @@ class TestRoster(TestCase):
             self.assertEqual(actual_results.getvalue(), expected)
         roster.close()
 
-    def test_email2(self): 
+    def test_search_email_roster_size_1_with_1_student_printed(self): 
         roster = open("roster.csv", 'w+', encoding = 'utf-8', newline="") #open the csv file
         roster.truncate(0)
         writer = csv.writer(roster)
@@ -110,7 +143,7 @@ class TestRoster(TestCase):
             self.assertEqual(actual_results.getvalue(), expected)
         read_roster.close()
 
-    def test_email3(self): 
+    def test_search_email_roster_size_5_with_some_students_printed(self): 
         roster = open("roster.csv", 'w+', encoding = 'utf-8', newline="") #open the csv file
         roster.truncate(0)
         writer = csv.writer(roster)
@@ -130,9 +163,75 @@ class TestRoster(TestCase):
             self.assertEqual(actual_results.getvalue(), expected)
         read_roster.close()
 
+    def test_search_email_roster_size_5_uppercase_pattern(self): 
+        roster = open("roster.csv", 'w+', encoding = 'utf-8', newline="") #open the csv file
+        roster.truncate(0)
+        writer = csv.writer(roster)
+        writer.writerow(["First Name", "Last Name", "Email", "GPA"])
+        writer.writerow(["Dan", "Hoang", "danhoang@ucdavis.edu", "3.0"])
+        writer.writerow(["John", "Smith", "jsmith@ucdavis.edu", "3.78"])
+        writer.writerow(["Joey", "Johnson", "jjohnson@ucdavis.edu", "2.2"])
+        writer.writerow(["Eddie", "Jacobs", "ejacobs4@ucdavis.edu", "4.0"])
+        writer.writerow(["Jimmy", "Graham", "jgraham@ucdavis.edu", "3.22"])
+        roster.close()
+        read_roster = open("roster.csv", 'r+', encoding = 'utf-8') #open the csv file
+        people_in_class = list(csv.reader(x.replace('\0', '') for x in read_roster))
+        expected = "Searching for all students with \"JO\" in their email without the \"@ucdavis.edu\" suffix:\n" 
+        expected = expected + "[\'Joey\', \'Johnson\', \'jjohnson@ucdavis.edu\', '2.2']\n"
+        with patch('sys.stdout', new = StringIO()) as actual_results:
+            search_commands.search_email("JO", people_in_class)
+            self.assertEqual(actual_results.getvalue(), expected)
+        read_roster.close()
+
+    def test_search_email_roster_size_5_pattern_appears_in_suffix(self): 
+        roster = open("roster.csv", 'w+', encoding = 'utf-8', newline="") #open the csv file
+        roster.truncate(0)
+        writer = csv.writer(roster)
+        writer.writerow(["First Name", "Last Name", "Email", "GPA"])
+        writer.writerow(["Dan", "Hoang", "danhoang@ucdavis.edu", "3.0"])
+        writer.writerow(["John", "Smith", "jsmith@ucdavis.edu", "3.78"])
+        writer.writerow(["Joey", "Johnson", "jjohnson@ucdavis.edu", "2.2"])
+        writer.writerow(["Eddie", "Jacobs", "ejacobs4@ucdavis.edu", "4.0"])
+        writer.writerow(["Jimmy", "Graham", "jgraham@ucdavis.edu", "3.22"])
+        roster.close()
+        read_roster = open("roster.csv", 'r+', encoding = 'utf-8') #open the csv file
+        people_in_class = list(csv.reader(x.replace('\0', '') for x in read_roster))
+        expected = "Searching for all students with \"davi\" in their email without the \"@ucdavis.edu\" suffix:\n" 
+        with patch('sys.stdout', new = StringIO()) as actual_results:
+            search_commands.search_email("davi", people_in_class)
+            self.assertEqual(actual_results.getvalue(), expected)
+        read_roster.close()
+
+    #--------------------------------------
+    #test the check_gpa_is_float() function
+    def test_check_gpa_is_float_with_invalid_gpa(self): 
+        roster = open("roster.csv", 'w+', encoding = 'utf-8', newline="") #open the csv file
+        expected = "Please enter gpa in the form of a float followed by an optional suffix of \'+\' or \'-\'\n"
+        with patch('sys.stdout', new = StringIO()) as actual_results:
+            search_commands.check_gpa_is_float("dab")
+            self.assertEqual(actual_results.getvalue(), expected)
+        roster.close()
+
+    
+    def test_check_gpa_is_float_with_valid_gpa(self): 
+        expected = ""
+        with patch('sys.stdout', new = StringIO()) as actual_result:
+            search_commands.check_gpa_is_float("3.2")
+            self.assertEqual(actual_result.getvalue(), expected)
+
     #--------------------------------------
     #test the search_gpa() function
-    def test_gpa0(self): 
+    def test_search_gpa_with_invalid_gpa(self): 
+        roster = open("roster.csv", 'w+', encoding = 'utf-8', newline="") #open the csv file
+        people_in_class = list(csv.reader(roster)) #students is a list of all students
+        #self.assertRaises(ValueError, search_commands.check_gpa_is_float, "dab")
+        expected = "Please enter gpa in the form of a float followed by an optional suffix of \'+\' or \'-\'\n"
+        with patch('sys.stdout', new = StringIO()) as actual_results:
+            search_commands.search_gpa("dab" , people_in_class)
+            self.assertEqual(actual_results.getvalue(), expected)   
+        roster.close()
+
+    def test_search_gpa_empty_roster(self): 
         roster = open("roster.csv", 'w+', encoding = 'utf-8', newline="") #open the csv file
         people_in_class = list(csv.reader(roster)) #students is a list of all students
         expected = "Searching roster for students with a gpa of 3.2:\n"
@@ -141,7 +240,7 @@ class TestRoster(TestCase):
             self.assertEqual(actual_results.getvalue(), expected)
         roster.close()
 
-    def test_gpa1(self): 
+    def test_search_gpa_roster_size_1_with_no_students_printed_floor(self): 
         roster = open("roster.csv", 'w+', encoding = 'utf-8', newline="") #open the csv file
         roster.truncate(0)
         writer = csv.writer(roster)
@@ -154,7 +253,20 @@ class TestRoster(TestCase):
             self.assertEqual(actual_results.getvalue(), expected)
         roster.close()
 
-    def test_gpa2(self): 
+    def test_search_gpa_roster_size_1_with_no_students_printed_ceiling(self): 
+        roster = open("roster.csv", 'w+', encoding = 'utf-8', newline="") #open the csv file
+        roster.truncate(0)
+        writer = csv.writer(roster)
+        writer.writerow(["First Name", "Last Name", "Email", "GPA"])
+        writer.writerow(["Dan", "Hoang", "danhoang@ucdavis.edu", '3.5'])
+        people_in_class = list(csv.reader(roster))
+        expected = "Searching roster for students with a gpa of 3.2 and below:\n"
+        with patch('sys.stdout', new = StringIO()) as actual_results:
+            search_commands.search_gpa("3.2-", people_in_class)
+            self.assertEqual(actual_results.getvalue(), expected)
+        roster.close()
+
+    def test_search_gpa_roster_size_1_with_1_student_printed_ceiling(self): 
         roster = open("roster.csv", 'w+', encoding = 'utf-8', newline="") #open the csv file
         roster.truncate(0)
         writer = csv.writer(roster)
@@ -169,7 +281,22 @@ class TestRoster(TestCase):
             self.assertEqual(actual_results.getvalue(), expected)
         read_roster.close()
 
-    def test_gpa3(self): 
+    def test_search_gpa_roster_size_1_with_1_student_printed_floor(self): 
+        roster = open("roster.csv", 'w+', encoding = 'utf-8', newline="") #open the csv file
+        roster.truncate(0)
+        writer = csv.writer(roster)
+        writer.writerow(["First Name", "Last Name", "Email", "GPA"])
+        writer.writerow(["Dan", "Hoang", "danhoang@ucdavis.edu", "3.5"])
+        roster.close()
+        read_roster = open("roster.csv", 'r+', encoding = 'utf-8') #open the csv file
+        people_in_class = list(csv.reader(x.replace('\0', '') for x in read_roster))
+        expected = "Searching roster for students with a gpa of 3.2 and above:\n" + "[\'Dan\', \'Hoang\', \'danhoang@ucdavis.edu\', '3.5']\n"
+        with patch('sys.stdout', new = StringIO()) as actual_results:
+            search_commands.search_gpa("3.2+", people_in_class)
+            self.assertEqual(actual_results.getvalue(), expected)
+        read_roster.close()
+
+    def test_search_gpa_roster_size_12_with_some_students_printed_equals(self): 
         roster = open("roster.csv", 'w+', encoding = 'utf-8', newline="") #open the csv file
         roster.truncate(0)
         writer = csv.writer(roster)
@@ -179,46 +306,27 @@ class TestRoster(TestCase):
         writer.writerow(["Joey", "Johnson", "jjohnson@ucdavis.edu", "2.2"])
         writer.writerow(["Eddie", "Jacobs", "ejacobs4@ucdavis.edu", "4.0"])
         writer.writerow(["Jimmy", "Graham", "jgraham@ucdavis.edu", "3.22"])
+        writer.writerow(["David", "Davidson", "ddavidson@ucdavis.edu", "3.0"])
+        writer.writerow(["Peter", "Peterson", "ppeterson@ucdavis.edu", "1.5"])
+        writer.writerow(["John", "Johnson", "jjohnson2@ucdavis.edu", "3.5"])
+        writer.writerow(["Jack", "Jackson", "jjackson@ucdavis.edu", "2.4"])
+        writer.writerow(["Ed", "Edison", "eedison@ucdavis.edu", "2.7"])
+        writer.writerow(["Harry", "Harrison", "hharrison@ucdavis.edu", "2.9"])
+        writer.writerow(["Larry", "Larson", "llarson@ucdavis.edu", "3.0"])
         roster.close()
         read_roster = open("roster.csv", 'r+', encoding = 'utf-8') #open the csv file
         people_in_class = list(csv.reader(x.replace('\0', '') for x in read_roster))
-        expected = "Searching roster for students with a gpa of 2.2:\n" 
-        expected = expected + "[\'Joey\', \'Johnson\', \'jjohnson@ucdavis.edu\', '2.2']\n"
+        expected = "Searching roster for students with a gpa of 3.0:\n" 
+        expected = expected + "[\'Dan\', \'Hoang\', \'danhoang@ucdavis.edu\', \'3.0\']\n"
+        expected = expected + "[\'David\', \'Davidson\', \'ddavidson@ucdavis.edu\', \'3.0\']\n"
+        expected = expected + "[\'Larry\', \'Larson\', \'llarson@ucdavis.edu\', \'3.0\']\n"
         with patch('sys.stdout', new = StringIO()) as actual_results:
-            search_commands.search_gpa("2.2", people_in_class)
+            search_commands.search_gpa("3.0", people_in_class)
             self.assertEqual(actual_results.getvalue(), expected)
         read_roster.close()
         roster.close()
 
-    def test_invalid_search_gpa(self): 
-        roster = open("roster.csv", 'w+', encoding = 'utf-8', newline="") #open the csv file
-        people_in_class = list(csv.reader(roster)) #students is a list of all students
-        #self.assertRaises(ValueError, search_commands.check_gpa_is_float, "dab")
-        expected = "Please enter gpa in the form of a float followed by an optional suffix of \'+\' or \'-\'\n"
-        with patch('sys.stdout', new = StringIO()) as actual_results:
-            search_commands.search_gpa("dab" , people_in_class)
-            self.assertEqual(actual_results.getvalue(), expected)   
-        roster.close()
-
-    #--------------------------------------
-    #test the check_gpa_is_float() function
-    def test_invalid_check_gpa_is_float(self): 
-        roster = open("roster.csv", 'w+', encoding = 'utf-8', newline="") #open the csv file
-        expected = "Please enter gpa in the form of a float followed by an optional suffix of \'+\' or \'-\'\n"
-        with patch('sys.stdout', new = StringIO()) as actual_results:
-            search_commands.check_gpa_is_float("dab")
-            self.assertEqual(actual_results.getvalue(), expected)
-        roster.close()
-
     
-    def test_check_valid_check_gpa_is_float(self): 
-        expected = ""
-        with patch('sys.stdout', new = StringIO()) as actual_result:
-            search_commands.check_gpa_is_float("3.2")
-            self.assertEqual(actual_result.getvalue(), expected)
-
-    
-
 
     
         
@@ -235,17 +343,6 @@ class TestRoster(TestCase):
 
 unittest.main()
 #roster.close()
-
-
-
-
-
-
-
-
-
-
-
 
 
 #f = open("result.csv",'w', encoding = 'utf-8')
